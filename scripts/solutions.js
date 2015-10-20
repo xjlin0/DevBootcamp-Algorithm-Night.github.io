04b127a7f036157f3c5ba8f5d3fb71a60e129053
John Akers <johnakersemail@gmail.com>
2015-10-20 13:26:08 -0700

    selector works, yay!

--- scripts/solutions.js
+++ scripts/solutions.js
@@ -9,11 +9,37 @@ function Main() {
     self.handleClick(e)
   })
 
-  this.handleClick = function( e ) {
+  //
+  this.handleClick = function(e) {
     if (e.target.className == 'person') {
       e.target.nextElementSibling.style.display = 'block'
     } else if (e.target.className == 'hide-answer') {
       console.log(e.target.parentElement.style.display = 'none')
+    } else if (e.target.id) {
+      if (e.target.id == 'all') {
+        self.showQuestions()
+      } else {
+        self.hideAllExcept(e.target.id)
+      }
     }
   }
+
+  this.showQuestions = function() {
+    var questions = document.querySelectorAll('.question')
+    for (var i = 0; i < questions.length; i++) {
+      questions[i].style.display = 'block'
+    }
+  }
+
+  this.hideAllExcept = function(className) {
+    var questions = document.querySelectorAll('.question')
+    for (var i = 0; i < questions.length; i++) {
+      if (questions[i].classList.contains(className)) {
+        questions[i].style.display = 'block'
+      } else {
+        questions[i].style.display = 'none'
+      }
+    }
+  }
+
 }
